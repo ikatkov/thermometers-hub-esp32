db621c4f4ca26bd5b24577f93bd6cd1fac958ddd
ikatkov <ikatkov@gmail.com>
2024-03-26 10:15:27 -0700

    Add readme

--- include/credentials_example.h
+++ include/credentials_example.h
@@ -4,4 +4,9 @@
 const char* ssid = "YOUR_SSID";
 const char* password = "YOUR_WIFI_PASSWORD";
 
+#define INFLUXDB_URL "type it here"
+#define INFLUXDB_TOKEN "type it here"
+#define INFLUXDB_ORG "type it here"
+#define INFLUXDB_BUCKET "type it here"
+
 #endif
